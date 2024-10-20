4206dcc5df3a370aec71432a35a8d9307e477fb7
Maria Kulik <mashaholod@ukr.net>
2024-10-20 19:38:50 +0300

    fix js

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,12 @@
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-menu]'),
   };
+  
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
 })();
