a33e3b969603491cb28cc1a530e6434b0f5b6b10
Maria Kulik <mashaholod@ukr.net>
2024-10-20 19:09:14 +0300

    add js

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector('[data-menu]'),
+  };
+})();
